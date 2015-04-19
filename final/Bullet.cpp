8b6612c3d142c1201e7cc7c7568b9773a17843b2
Travis Patterson <tpatter2@nd.edu>
2015-04-19 16:07:57 -0400

    Update Bullet.cpp

--- final/Bullet.cpp
+++ final/Bullet.cpp
@@ -1,10 +1,17 @@
 #include "Bullet.h"
+#include "Enemy.h"
+#include "Game.h"
+#include <typeinfo>
 #include <QTimer>
+#include <QGraphicsScene>
+#include <QDebug>
+#include <QList>
 
-Bullet::Bullet()
+extern Game * game; //allows use of the external global object
+Bullet::Bullet(QGraphicsItem *parent): QObject(),QGraphicsPixmapItem(parent)
 {
-    //draw the rect
-    setRect(0,0,10,50);
+    //draw the graphic
+    setPixmap(QPixmap(":/images/greenLaserRay.png"));
 
     // connect
     QTimer * timer = new QTimer();
@@ -15,6 +22,29 @@ Bullet::Bullet()
 
 void Bullet::move()
 {
+    //check collision w/ enemy
+    QList<QGraphicsItem *> colliding_items = collidingItems();
+    for (int i = 0, n = colliding_items.size(); i < n; ++i){
+        if (typeid(*(colliding_items[i])) == typeid(Enemy)){
+            //increase score
+            game->score->increase();
+
+            //remove items
+            scene()->removeItem(colliding_items[i]);
+            scene()->removeItem(this);
+            //delete items
+            delete colliding_items[i];
+            delete this;
+            return;
+        }
+    }
+
     // move the bullet up
     setPos(x(),y()-10);
+    // delete the bullet when it moves off screen
+    if(pos().y() < -50){
+//        qDebug() << "bullet deleted";
+        scene()->removeItem(this);
+        delete this;
+    }
 }
