97a201485eeb0d8edc8140f4da2e2851fffc7575
Travis Patterson <tpatter2@nd.edu>
2015-04-19 16:12:49 -0400

    Create Health.h
    Class that takes care of player health

---
+++ final/Health.h
@@ -0,0 +1,16 @@
+#ifndef HEALTH
+#define HEALTH
+
+#include <QGraphicsTextItem>
+
+class Health: public QGraphicsTextItem{
+public:
+    Health(QGraphicsItem * parent=0);
+    void decrease();
+    int getHealth();
+private:
+    int health;
+};
+
+#endif // HEALTH
+
