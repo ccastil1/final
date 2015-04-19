cae8f0e9cf3dbb9175acd07884ba655d88057299
Travis Patterson <tpatter2@nd.edu>
2015-04-19 16:23:19 -0400

    Update Game.cpp
    Will probably crash if there is no matching file/file path as line 16.

--- final/Game.cpp
+++ final/Game.cpp
@@ -13,7 +13,7 @@ Game::Game(QWidget *parent){
     scene = new QGraphicsScene();
 //    view = new QGraphicsView();
     scene->setSceneRect(0,0,800,600);   //set the QGraphicsScene to a specified size
-    setBackgroundBrush(QBrush(QImage(":/images/star-background.png")));
+    setBackgroundBrush(QBrush(QImage(":/images/star-background.png"))); //change filename to change background
 
     //make newly created scene
     setScene(scene);
@@ -50,32 +50,3 @@ Game::Game(QWidget *parent){
 
     show();
 }
-//    // create an item to put into the scene
-//    Player * player = new Player();
-//    player->setRect(0,0,100,100);
-
-//    // add item to the scene
-//    scene->addItem(player);
-
-//    // make player focusable
-//    player->setFlag(QGraphicsItem::ItemIsFocusable);
-//    player->setFocus();
-
-//    // add a view
-//    QGraphicsView * view = new QGraphicsView(scene);
-//    view -> setScene(scene);
-
-//    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
-//    view->show();
-
-//    view->setFixedSize(800,600);    //sets the QGraphicsView (game window) to a set size
-
-//    player->setPos(view->width()/2,view->height()-player->rect().height()); //Set the x,y position of the player
-
-//    //spawn enemies
-//    QTimer * timer = new QTimer();
-//    QObject::connect(timer,SIGNAL(timeout()),player,SLOT(spawn()));
-//    timer->start(1750);
-//}
