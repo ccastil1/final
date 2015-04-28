87abe9585aa0f6f634f6a0e2706c24fa7e868b80
Travis Patterson <tpatter2@nd.edu>
2015-04-28 12:32:15 -0400

    Update game.h
    added createEnemies function, createRoad function, spawnEnemy function.
    Also added data members to keep track of enemies and points for enemies to follow.

--- final/Tower Defense/game.h
+++ final/Tower Defense/game.h
@@ -2,19 +2,30 @@
 #define GAME
 #include <QGraphicsView>
 #include <QMouseEvent>
+#include <QGraphicsPixmapItem>
 #include "tower.h"
 
 class Game: public QGraphicsView{
+    Q_OBJECT
 public:
     //member functions
     Game();
     void setCursor(QString filename);
     void mouseMoveEvent(QMouseEvent *event);
     void mousePressEvent(QMouseEvent *event);
+    void createEnemies(int numberOfEnemies);
+    void createRoad();
+
     //member attributes
     QGraphicsScene * scene;
     QGraphicsPixmapItem * cursor;
     Tower * build;
+    QTimer * spawnTimer;
+    int enemiesSpawned;
+    int maxNumOfEnemies;
+    QList<QPointF> ptsToFollow;
+public slots:
+    void spawnEnemy();
 };
 
 #endif // GAME
